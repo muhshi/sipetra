1530841c64f4bac4115cd9ee4a42989349501709
asmuammal <100674458+asmuam@users.noreply.github.com>
2026-04-10 10:16:16 +0700

    custom login

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -16,7 +16,7 @@
         </button>
         <button wire:click="$set('loginType', 'email')" 
             class="flex-1 py-2 text-sm font-medium rounded-md transition-all {{ $loginType === 'email' ? 'bg-white shadow text-blue-700' : 'text-gray-500 hover:text-gray-700' }}">
-            Mitra / Admin (Email)
+            Mitra (Email)
         </button>
     </div>
 
