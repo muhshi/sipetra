795e37a644597589a41eb6d3199d226fa40f542c
asmuammal <100674458+asmuam@users.noreply.github.com>
2026-04-15 11:09:27 +0700

    feat: access policy wizard

--- app/Filament/Resources/Passport/Pages/CreateClient.php
+++ app/Filament/Resources/Passport/Pages/CreateClient.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\Passport\Pages;
 
+use App\Enums\ClientAccessPolicy;
 use App\Filament\Passport\Schemas\ExtendedClientWizardForm;
 use App\Filament\Resources\Passport\ClientResource;
 use Filament\Schemas\Schema;
+use Illuminate\Database\Eloquent\Model;
 use N3XT0R\FilamentPassportUi\Resources\ClientResource\Pages\CreateClient as BaseCreateClient;
 
 class CreateClient extends BaseCreateClient
@@ -16,5 +18,15 @@ class CreateClient extends BaseCreateClient
     public function form(Schema $schema): Schema
     {
         return ExtendedClientWizardForm::configure($schema);
+    }
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $record = parent::handleRecordCreation($data);
+        $record->forceFill([
+            'access_policy' => $data['access_policy'] ?? ClientAccessPolicy::Restricted->value,
+        ])->save();
+
+        return $record;
     }
 }
