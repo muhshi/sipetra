4cc54646195e1911afc1b61b7e9f8b6c45edc052
asmuammal <100674458+asmuam@users.noreply.github.com>
2026-04-15 11:23:24 +0700

    feat: seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleAndPermissionSeeder::class,
             AdminUserSeeder::class,
+            PassportScopeSeeder::class,
+            ImportUsersSeeder::class,
         ]);
     }
 }
