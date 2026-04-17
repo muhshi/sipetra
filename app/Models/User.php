e09ac26dfd06eedea3aab9dba7366279ae4e0dce
asmuammal <100674458+asmuam@users.noreply.github.com>
2026-04-17 13:22:04 +0700

    fix: use public disk for filament avatar url

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,7 +50,7 @@ class User extends Authenticatable implements FilamentUser, HasAvatar, HasPasspo
      */
     public function getFilamentAvatarUrl(): ?string
     {
-        return $this->avatar_url ? Storage::url($this->avatar_url) : null;
+        return $this->avatar_url ? Storage::disk('public')->url($this->avatar_url) : null;
     }
 
     /**
