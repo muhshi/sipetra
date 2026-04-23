eeae09d9c8071297e6c616e34a7cdc994c63dca7
asmuammal <100674458+asmuam@users.noreply.github.com>
2026-04-23 17:13:46 +0700

    emptyStateMessage

--- app/Filament/Passport/Schemas/ExtendedClientWizardForm.php
+++ app/Filament/Passport/Schemas/ExtendedClientWizardForm.php
@@ -91,14 +91,9 @@ class ExtendedClientWizardForm extends ClientWizardForm
                         ->required()
                         ->multiple()
                         ->searchable()
-                        ->searchPrompt('Ketik untuk mencari...')
+                        ->searchPrompt('Mulai mengetik untuk mencari...')
                         ->loadingMessage('Sedang mencari...')
                         ->noSearchResultsMessage('Tidak ditemukan.')
-                        ->emptyOptionsMessage(fn (Get $get): string => 
-                            $this->normalizeRuleType($get('rule_type')) === AccessRuleType::User 
-                                ? 'Silakan ketik nama/NIP untuk mencari...' 
-                                : 'Tidak ada pilihan.'
-                        )
                         ->options(fn (Get $get): array => $this->getInitialRuleValueOptions($get('rule_type')))
                         ->getSearchResultsUsing(fn (Get $get, ?string $search): array => $this->searchRuleValueOptions($get('rule_type'), $search))
                         ->getOptionLabelsUsing(fn (Get $get, array $values): array => $this->getRuleValueLabels($get('rule_type'), $values))
