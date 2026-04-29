548b54e38301fe3b5bef98d65107d85a614a33f2
muhshi <amuhshi@gmail.com>
2026-04-29 16:11:35 +0700

    fix: manually register ImageOptimizerServiceProvider to ensure macros are loaded

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    DaniHidayatX\ImageOptimizer\ImageOptimizerServiceProvider::class,
 ];
