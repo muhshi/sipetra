a6acc9b29a0b35cdf98ba850bbd4a22afeb85b92
muhshi <amuhshi@gmail.com>
2026-04-29 16:15:58 +0700

    chore: cleanup manual registration now that package is installed in container

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Providers\AppServiceProvider;
+use App\Providers\Filament\AdminPanelProvider;
+
 return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
-    DaniHidayatX\ImageOptimizer\ImageOptimizerServiceProvider::class,
+    AppServiceProvider::class,
+    AdminPanelProvider::class,
 ];
