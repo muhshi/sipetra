aab6d814d9d3ac11528798ce84fda493248a3cc7
muhshi <muhshi>
2026-05-06 15:09:03 +0700

    Update PortalAppsSeeder with icons and run seeder

--- database/seeders/PortalAppsSeeder.php
+++ database/seeders/PortalAppsSeeder.php
@@ -13,14 +13,14 @@ class PortalAppsSeeder extends Seeder
     public function run(): void
     {
         $apps = [
-            ['name' => 'Alfath', 'url' => 'https://alfath.bpsdemak.com/', 'order' => 1],
-            ['name' => 'Magang', 'url' => 'http://magang.bpsdemak.com/', 'order' => 2],
-            ['name' => 'CKP', 'url' => 'https://ckp.bpsdemak.com/', 'order' => 3],
-            ['name' => 'Surat', 'url' => 'https://surat.bpsdemak.com/', 'order' => 4],
-            ['name' => 'Siputri', 'url' => 'https://siputri.bpsdemak.com/', 'order' => 5],
-            ['name' => 'Demakai', 'url' => 'https://demakai.bpsdemak.com/', 'order' => 6],
-            ['name' => 'Portal', 'url' => 'https://portal.bpsdemak.com/', 'order' => 7],
-            ['name' => 'Dinamit', 'url' => 'https://dinamit.bpsdemak.com/', 'order' => 8],
+            ['name' => 'Alfath', 'url' => 'https://alfath.bpsdemak.com/', 'order' => 1, 'icon' => 'heroicon-o-academic-cap'],
+            ['name' => 'Magang', 'url' => 'http://magang.bpsdemak.com/', 'order' => 2, 'icon' => 'heroicon-o-briefcase'],
+            ['name' => 'CKP', 'url' => 'https://ckp.bpsdemak.com/', 'order' => 3, 'icon' => 'heroicon-o-document-chart-bar'],
+            ['name' => 'Surat', 'url' => 'https://surat.bpsdemak.com/', 'order' => 4, 'icon' => 'heroicon-o-envelope'],
+            ['name' => 'Siputri', 'url' => 'https://siputri.bpsdemak.com/', 'order' => 5, 'icon' => 'heroicon-o-identification'],
+            ['name' => 'Demakai', 'url' => 'https://demakai.bpsdemak.com/', 'order' => 6, 'icon' => 'heroicon-o-shopping-bag'],
+            ['name' => 'Portal', 'url' => 'https://portal.bpsdemak.com/', 'order' => 7, 'icon' => 'heroicon-o-home'],
+            ['name' => 'Dinamit', 'url' => 'https://dinamit.bpsdemak.com/', 'order' => 8, 'icon' => 'heroicon-o-fire'],
         ];
 
         foreach ($apps as $app) {
@@ -29,6 +29,7 @@ class PortalAppsSeeder extends Seeder
                 [
                     'name' => $app['name'],
                     'order' => $app['order'],
+                    'icon' => $app['icon'],
                     'is_active' => true,
                     'description' => 'Sistem Informasi ' . $app['name'] . ' BPS Kabupaten Demak',
                 ]
